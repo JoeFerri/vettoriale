706439608c22322736da628baf981171419077c5
Giuseppe Ferri <46257651+JoeFerri@users.noreply.github.com>
2021-03-18 09:46:58 +0100

    Create print.test.ts

---
+++ tests/print.test.ts
@@ -0,0 +1,45 @@
+/**
+ * @author Giuseppe Ferri
+ */
+
+
+
+import * as v from "../lib/index";
+import { expect, should } from 'chai';
+// require('chai').should()
+should();
+ 
+ 
+  describe(`modulo print`, function() {
+
+    let
+      a = `1,2,3,4,5`,
+      b = `1,2,3\n4,5,6`,
+      c = `22`,
+      d = `1,2,3,4\n1,2\n1,2,3,4,5\n1,2`;
+
+    it(``, function() {
+      
+      ("|" + v.printOnOneLine(d,' + ',b,' - ',c,' = ',a) + "|")
+        .should.to.be
+        .equal(`|1,2,3,4                           \n1,2       + 1,2,3 - 22 = 1,2,3,4,5\n1,2,3,4,5   4,5,6                 \n1,2                               |`);
+      
+      ("|" + v.printOnOneLine(b,' - ',b) + "|")
+        .should.to.be
+        .equal(`|1,2,3 - 1,2,3\n4,5,6   4,5,6|`);
+
+      ("|" + v.printOnOneLine('+') + "|")
+      .should.to.be
+      .equal(`|+|`);
+
+      ("|" + v.printOnOneLine('') + "|")
+      .should.to.be
+      .equal(`||`);
+
+      ("|" + v.printOnOneLine() + "|")
+      .should.to.be
+      .equal(`||`);
+
+    });
+
+});
